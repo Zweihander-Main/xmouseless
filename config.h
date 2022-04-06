e9f24df79286c0907e6f383fef6cc945433773d1
Zweihänder <zweidev@zweihander.me>
2022-04-05 23:55:51 -0400

    feat(config): basic config for keynav

--- config.h
+++ config.h
@@ -20,10 +20,14 @@ static SpeedBinding speed_bindings[] = {
  * you can also add any other direction (e.g. diagonals) */
 static MoveBinding move_bindings[] = {
     /* key         x      y */
-    { XK_j,        -1,     0 },
+    { XK_h,        -1,     0 },
     { XK_l,         1,     0 },
-    { XK_i,         0,    -1 },
-    { XK_k,         0,     1 },
+    { XK_k,         0,    -1 },
+    { XK_j,         0,     1 },
+    { XK_a,        -1,     0 },
+    { XK_d,         1,     0 },
+    { XK_w,         0,    -1 },
+    { XK_s,         0,     1 },
 };
 
 /* 1: left
@@ -31,7 +35,6 @@ static MoveBinding move_bindings[] = {
  * 3: right */
 static ClickBinding click_bindings[] = {
     /* key         button */  
-    { XK_space,    1 },
     { XK_f,        1 },
     { XK_d,        2 },
     { XK_s,        3 },
@@ -45,18 +48,19 @@ static ScrollBinding scroll_bindings[] = {
     { XK_p,        0 ,   -25 },
     { XK_plus,     0 ,    80 },
     { XK_minus,    0 ,   -80 },
-    { XK_h,        25,    0  },
-    { XK_g,       -25,    0  },
+    { XK_comma,  -25 ,    0  },   
+    { XK_period,  25 ,    0  },  
 };
 
 /* executes shell commands */
 static ShellBinding shell_bindings[] = {
     /* key         command */  
-    { XK_b,        "wmctrl -a firefox" },
-    { XK_0,        "xdotool mousemove 0 0" },
+    { XK_g,        "xdotool mousedown 1" },   // highlight | drag&drop (begin)
+    { XK_t,        "xdotool mouseup 1" },     // highlight | drag&drop (end)
 };
 
 /* exits on key release which allows click and exit with one key */
 static KeySym exit_keys[] = {
-    XK_Escape, XK_q, XK_space
+    XK_Escape, XK_q
 };
+
